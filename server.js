a1808524542e854a3668647628d396589b2c4a92
hhx2778 <3435127442@qq.com>
2026-08-28 19:06:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.get('/', (req,res)=>res.sendFile(__dirname+'/public/index.html'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT,()=>{
-    console.log("service running");
+app.get('/api/questions', (req, res) => {
+  res.json({
+    questions: [],
+    examDuration: 1800000
+  });
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+
+app.listen(port, ()=>{});
