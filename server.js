aaec066b1261df71e0f27794822ed5f96c968d3a
hhx2778 <3435127442@qq.com>
2026-08-28 19:26:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+// 静态资源优先，所有html直接可访问
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 内存存储答卷
 let answerList = [];
+const ADMIN_PASS = "123456";
 
+//题目接口
 app.get('/api/questions', (req, res) => {
   res.json({
     questions: [],
@@ -15,20 +19,23 @@ app.get('/api/questions', (req, res) => {
   });
 });
 
+//提交答卷
 app.post('/api/submit', (req, res) => {
   answerList.push(req.body);
   res.json({ ok: true });
 });
 
-const ADMIN_PASS = "123456";
+//管理员登录
 app.post('/api/admin/login', (req,res)=>{
-  res.json({ok: req.body.pwd === ADMIN_PASS, token:"ok"});
+  res.json({ok: req.body.pwd === ADMIN_PASS});
 });
 
+//获取答卷
 app.get('/api/admin/list', (req,res)=>{
   res.json(answerList);
 });
 
+//兜底只拦截未定义路径
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public','index.html'));
 });
